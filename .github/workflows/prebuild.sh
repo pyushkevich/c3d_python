f552adc713c571b1f81b32904c1345750771a405
Paul Yushkevich <pyushkevich@gmail.com>
2024-08-07 13:00:37 -0400

    separate windows and linux workflows

---
+++ .github/workflows/prebuild.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -x -e 
+
+mkdir -p buildext/install && cd buildext
+cmake \
+    -DFETCH_DEPENDENCIES=ON \
+    -DDEPENDENCIES_ONLY=ON \
+    -DCMAKE_INSTALL_PREFIX=./install \
+    ..
+make -j$(nproc) && make install
---
+++ .github/workflows/prebuild_win.bash
@@ -0,0 +1,18 @@
+@echo on
+setlocal enabledelayedexpansion
+
+REM Create build and install directories
+mkdir buildext\install
+cd buildext
+
+REM Run CMake to configure the project
+cmake ^
+    -DFETCH_DEPENDENCIES=ON ^
+    -DDEPENDENCIES_ONLY=ON ^
+    -DCMAKE_INSTALL_PREFIX=.\install ^
+    ..
+
+REM Build the project using all available processors
+cmake --build . --target install -- -j %NUMBER_OF_PROCESSORS%
+
+endlocal
