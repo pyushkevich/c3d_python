dd868ab4f28c5bb4f0919e085e431d2ec931f8e0
Paul Yushkevich <pyushkevich@gmail.com>
2024-08-09 06:20:59 -0400

    first time it worked on windows

--- .github/workflows/prebuild_win.bat
+++ .github/workflows/prebuild_win.bat
@@ -5,13 +5,21 @@ REM Create build and install directories
 mkdir be\install
 cd be
 
+REM Read the architecture parameter
+REM Check the first command-line parameter
+if "%1" == "AMD64" (
+    set ARCH=x64
+) else (
+    set ARCH=Win32
+)
+
 REM Run CMake to configure the project
 cmake ^
     -DFETCH_DEPENDENCIES=ON ^
     -DDEPENDENCIES_ONLY=OFF ^
     -DCMAKE_INSTALL_PREFIX=.\install ^
     -DCMAKE_BUILD_TYPE=Release ^
-    -A x64 ^
+    -A %ARCH% ^
     ..
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 
